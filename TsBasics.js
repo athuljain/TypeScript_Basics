933f75f1b6f69814a38aba79fa8fc7569419c336
athuljain <athuljainkj2@gmail.com>
2024-05-20 10:55:37 +0530

    first commit

---
+++ TsBasics.js
@@ -0,0 +1,7 @@
+// infer types or implicit types
+// let userName="athul"
+// //  userName=0   // automatically shows error and automatically shows type
+var userDetails = {
+    name: "athul",
+    age: "24"
+};
---
+++ TsBasics.ts
@@ -0,0 +1,85 @@
+// infer types or implicit types
+// let userName="athul"
+// //  userName=0   // automatically shows error and automatically shows type
+
+
+// Defining types / Expicit type
+
+
+// let userName:string="athul"
+// let userNumber:number=30000
+
+// let isSubscribed:boolean=true
+
+// let skills=['js','html','react','node','react']
+
+// let skills:string[]=['js','html','react','node','react']
+
+// let count: number[]=[1,2,3]
+// let emptyArray:[]=[]
+
+// let emptyArray=[]
+
+
+// let userDetails:{
+//     name:string,
+//     age:number,
+
+// }={'name':'athul','age':23}
+
+
+
+
+//Interface
+
+// let userDetails:{name:string,age:number,salary:number}={name:'athul',age:24,salary:20000}
+
+
+// interface Details{name:string,age:number,salary:number}
+
+// let userDetails:Details={name:'athul',age:24,salary:20000}
+
+// let adminDetails:Details={name:'athul',age:24,salary:20000}
+
+
+
+
+ 
+// Type
+
+// type Details={
+//     name:String;age:Number;getName:()=>void
+// }
+
+// let userDetails:Details={name:"Athul",age:24,getName() {
+//     console.log(this.name)
+// },}
+
+// Type define premitive data types , union types, intersection types and more
+
+//Union Type
+
+
+// type Details={
+//     name:String,
+//     age:Number | String
+// }
+
+// let userDetails: Details={
+//     name:"athul",
+//     age:"24"
+// }
+
+
+
+// // Union type use in Array
+
+// let Skills:(string|number)[]=["js","html",24]
+
+
+// //Optional Operator
+
+// type Details={
+//     name:String,age:number|string,
+//     getName?:()=>void
+// }
